6d30df73dd46e0be3a028a162b47bdae45426ad9
Анна Желтова <annazeltova@MacBook-Pro-Anna.local>
2020-04-07 18:38:40 +0300

    Updates

---
+++ precache-manifest.a21ff128ffc5e17e0d0b183e431c2aa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72bfde0b0d8f77e11cede28ec5a262e9",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "83bca1ce44a19de2b5de",
+    "url": "/test/static/css/main.086c4042.chunk.css"
+  },
+  {
+    "revision": "2da0e65e9886eb77e567",
+    "url": "/test/static/js/2.083786a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.083786a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83bca1ce44a19de2b5de",
+    "url": "/test/static/js/main.2e5d18f4.chunk.js"
+  },
+  {
+    "revision": "117f89bf047e1e972970",
+    "url": "/test/static/js/runtime-main.62c55eac.js"
+  },
+  {
+    "revision": "50996d777ccb4539aaf90e78f76fa683",
+    "url": "/test/static/media/photo-1491972690050-ba117db4dc09 1.50996d77.png"
+  }
+]);
